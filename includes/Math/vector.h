cb0527e5d040fb3e2e3ad75a7ca3e8bb849f8cd2
Kartikeyan Iyer <77189531+kartechkeyan98@users.noreply.github.com>
2023-06-07 21:23:00 +0530

    Add files via upload
    By Daddy

--- includes/Math/vector.h
+++ includes/Math/vector.h
@@ -1,29 +1,90 @@
 #pragma once
-#include <iostream>
+#include<iostream>
+#include<cmath>
 
-class Vector3
-{
+class Vector3{
     public:
-    float x = 0;
-    float y = 0;
-    float z = 0;
+        float x;
+        float y;
+        float z;
 
-    Vector3(){};
-    ~Vector3(){};
+        Vector3(float x, float y, float z){
+            this->x=x;
+            this->y=y;
+            this->z=z;
+        }
 
-    Vector3(float x, float y, float z)
-    {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
+        // length
+        double length(){
+            return sqrt((this->x)*(this->x) + (this->y)*(this->y) + (this->z)*(this->z));
+        } 
 
-    void print()
-    {
-        std::cout<< "X: " << this->x <<" Y: "<< this->y <<" Z: "<< this->z <<std::endl;
-    }
+        // scalar multiplication: a*vec an vec*a
+        friend Vector3 operator *(float a,Vector3 v){
+            Vector3 res(0,0,0);
+            res.x=a*v.x;
+            res.y=a*v.y;
+            res.z=a*v.z;
+            return res;
+        }
+        friend Vector3 operator *(Vector3 v,float a){
+            Vector3 res(0,0,0);
+            res.x=a*v.x;
+            res.y=a*v.y;
+            res.z=a*v.z;
+            return res;
+        }
 
-    // Start Your Assignment Here.
+        // dot product multiplication
+        //member function method
+        double dot(Vector3 &v){
+            double dot=(this->x)*v.x+(this->y)*v.y+(this->z)*(v.z);
+            return dot;
+        }
+        //operator overload way
+        double operator*(Vector3 &v){
+            double dot=(this->x)*v.x+(this->y)*v.y+(this->z)*(v.z);
+            return dot;
+        }
 
+        // corss product
+        Vector3 cross(Vector3 &v){
+            Vector3 res(0,0,0);
+            res.x=(this->y)*v.z - (this->z)*v.y;
+            res.y=(this->z)*v.x - (this->x)*v.x;
+            res.z=(this->x)*v.y - (this->y)*v.y;
+            return res;
+        }
+
+        // addition by operator overload
+        Vector3 operator +(Vector3 &v){
+            Vector3 res(0,0,0);
+            res.x=(this->x)+v.x;
+            res.y=(this->y)+v.y;
+            res.z=(this->z)+v.z;
+            return res;
+        }
+
+        //division by operator overload
+        Vector3 operator /(float a){
+            Vector3 res(0,0,0);
+            res.x=(this->x)/a;
+            res.y=(this->y)/a;
+            res.z=(this->z)/a;
+            return res;
+        }
+
+        //subtraction by operator overload
+        Vector3 operator -(Vector3 &a){
+            Vector3 res(0,0,0);
+            res.x=this->x-a.x;
+            res.y=this->y-a.y;
+            res.z=this->z-a.z;
+            return res;
+        }
+
+        //print
+        void print(){
+            printf("(%0.3f, %0.3f, %0.3f)\n",this->x,this->y,this->z);
+        }
 };
-
--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
+#include<iostream>
+#include<cmath>
 #include "vector.h"
+using namespace std;
 
-int main()
-{
-    std::cout<<"Assignment One Evaluation"<<std::endl;
+int main(void){
+    Vector3 v1(1.5,1.2,1.8);
+    Vector3 v2(2.5,3.6,5.1);
+    
+    printf("Length of v1: %0.3f\n",v1.length());
+    printf("Length of v2: %0.3f\n",v2.length());
+    cout<<"2*v1 = "; (2*v1).print();
+    cout<<"v1*2 = "; (v1*2).print();
+    printf("v1.v2 = %0.3f\n",v1.dot(v2));
+    printf("v1.v2 = %0.3f\n",v1*v2);
+    cout<<"v1 X v2 = "; (v1.cross(v2)).print();
+    cout<<"v1 + v2 = "; (v1+v2).print();
+    cout<<"v1/2 = "; (v1/2).print();
+    cout<<"v1 - v2 = "; (v1-v2).print();
 
-    Vector3 vec1(1,2,3);
-    vec1.print();
-    std::cin.get();
-    return 0;
+
 }
